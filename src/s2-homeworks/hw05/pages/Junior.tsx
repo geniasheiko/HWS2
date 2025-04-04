d916f94ce2a40bcc436012cd0b9b830409422b99
Genia Melnyk <geniasheiko@gmail.com>
2025-04-04 14:53:04 +0300

    HomeWork10

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -7,10 +7,10 @@ import React from 'react'
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-            <HW6 />
+            {/* <HW6 /> */}
             {/* <HW7 /> */}
-            <HW8 />
-            <HW9 />
+            {/* <HW8 />
+            <HW9 /> */}
         </div>
     )
 }
