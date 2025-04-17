4a4b1e1a39be76bbaf44f87dd491ce0f3920bfda
Genia Melnyk <geniasheiko@gmail.com>
2025-04-17 16:42:54 +0300

    Added new home work.

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,22 @@
+export type ThemeType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action:ThemeType ):typeof initState => { // fix any
     switch (action.type) {
         // дописать
-
+case 'SET_THEME_ID':
+    return {
+        ...state, themeId: action.id
+    }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number):ThemeType  => ({ type: 'SET_THEME_ID', id }) // fix any
