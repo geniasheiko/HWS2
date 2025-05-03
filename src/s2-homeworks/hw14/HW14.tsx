d9cc84a8760ec220d4da7aca4f9609d1674e2dd3
Genia Melnyk <geniasheiko@gmail.com>
2025-05-03 08:58:49 +0300

    moved filtered data in getTechs function.

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import SuperDebouncedInput from "./common/c8-SuperDebouncedInput/SuperDebouncedInput";
+import React, { useEffect, useState } from 'react'
 import s2 from '../../s1-main/App.module.css'
 import s from './HW14.module.css'
+import axios from 'axios'
+import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
+import { useSearchParams } from 'react-router-dom'
 
 /*
 * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
@@ -13,54 +13,73 @@ import s from './HW14.module.css'
 * 5 - добавить HW14 в HW5/pages/JuniorPlus
 * */
 
-
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
             'https://samurai.it-incubator.io/api/3.0/homework/test2',
             { params: { find } }
         )
-        .then((res) => {
-            return { data: { techs: res.data.techs.filter((tech) => tech.includes(find)) } }; // Фильтруем данные
-        })
+        //!!
+        // .then((res) => {
+        //     // Фильтруем данные на клиенте, если сервер возвращает лишние элементы
+        //     return { data: { techs: res.data.techs.filter((tech) => tech.includes(find)) } };
+        // })
         .catch((e) => {
-            alert(e.response?.data?.errorText || e.message);
-            return { data: { techs: [] } }; // Возвращаем пустой массив techs в случае ошибки
-        });
-};
+            alert(e.response?.data?.errorText || e.message)
+            //!!
+           return { data: { techs: [] } }
+        })
+}
 
 const HW14 = () => {
-    const [find, setFind] = useState('');
-    const [isLoading, setLoading] = useState(false);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [techs, setTechs] = useState<string[]>([]);
+    const [find, setFind] = useState('')
+    const [isLoading, setLoading] = useState(false)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [techs, setTechs] = useState<string[]>([])
 
     const sendQuery = (value: string) => {
-        setLoading(true);
+        setLoading(true)
         getTechs(value)
             .then((res) => {
-                setTechs(res?.data.techs || []); // Устанавливаем только отфильтрованные данные
+                if (res?.data.techs) {
+                    const filteredTechs = res.data.techs.filter((tech) => 
+                    tech.toLowerCase().includes(value.toLowerCase())
+                );
+                    setTechs(filteredTechs)
+                } else {
+                    setTechs([])
+                }
+                // делает студент
+
+                // сохранить пришедшие данные
+
+                //
             })
-            .finally(() => setLoading(false));
-    };
+            .finally(() => setLoading(false))
+    }
 
     const onChangeText = (value: string) => {
-        setFind(value);
-        setSearchParams({ find: value }); // Обновляем параметры URL
-    };
+        setFind(value)
+        // делает студент
+        const params = Object.fromEntries(searchParams)// извлекает текущие параметры из URL и преобразует их в объект
+        params.find = value// обновляет или добавляет параметр 'find' в этот объект
+        // добавить/заменить значение в квери урла
+        // setSearchParams(
+        setSearchParams({ find: value })// заменяет параметры URL, устанавливая 'find' равным введённому значению
+        //
+    }
 
     useEffect(() => {
-        const params = Object.fromEntries(searchParams); // Преобразуем параметры URL в объект
-        const findValue = params.find || ''; // Если параметр find отсутствует, используем пустую строку
-        sendQuery(findValue); // Загружаем данные
-        setFind(findValue); // Устанавливаем значение в поле ввода
-    }, [searchParams]);
+        const params = Object.fromEntries(searchParams) //Object.fromEntries(searchParams) превращает его в обычный объект JS,
+        sendQuery(params.find || '')
+        setFind(params.find || '')
+    }, [searchParams])
 
-    const mappedTechs = techs.map((t) => (
+    const mappedTechs = techs.map(t => (
         <div key={t} id={'hw14-tech-' + t} className={s.tech}>
             {t}
         </div>
-    ));
+    ))
 
     return (
         <div id={'hw14'}>
@@ -81,7 +100,7 @@ const HW14 = () => {
                 {mappedTechs}
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default HW14;
+export default HW14
